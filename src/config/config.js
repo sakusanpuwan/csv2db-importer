64fd8964864d2d0a671e081febccd67a5599cae1
sakusanpuwan <sakusanpuwan98@gmail.com>
2025-05-13 17:56:35 +0100

    Add CSV file processing, db table creation with dynamic headers & data insertion

--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,5 @@ module.exports = {
     DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     NODE_ENV: process.env.NODE_ENV || 'development',
     FILE_PATH: process.env.FILE_PATH || './data/imdb_top_250.csv',
+    TABLE_NAME: process.env.TABLE_NAME || 'TOP_MOVIES',
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,69 @@
 const knexdb = require("./config/knexdb.js");
 const config = require("./config/config.js");
+const fs = require("fs");
+const csvParser = require("csv-parser");
 
-console.log('DB_USER:', config.DB_USER);
-console.log('DB_PASSWORD:', config.DB_PASSWORD);
-console.log('DB_CONNECTION_STRING:', config.DB_CONNECTION_STRING);
-console.log('FILE_PATH:', config.FILE_PATH);
+const filePath = config.FILE_PATH;
+const tableName = config.TABLE_NAME;
+const headers = [];
+const rows = [];
 
 async function main() {
-    const rows = await knexdb.select('*').from('MOVIES');
-    console.table(rows);
-    await knexdb.destroy(); // Close the database connection
+    fs.createReadStream(filePath)
+        .pipe(csvParser())
+        .on("headers", (headerList) => {
+            headerList.forEach((header) => {
+                headers.push(header);
+            });
+        })
+        .on("data", (row) => {
+            rows.push(row);
+        })
+        .on("end", async () => {
+            console.log("CSV file successfully processed");
+            try {
+                await createTable();
+                await insertData();
+                knexdb.destroy(); // Close the database connection
+            } catch (error) {
+                console.error("Error interacting with database:", error);
+            }
+
+        })
+        .on("error", (error) => {
+            console.error("Error processing CSV file:", error);
+        });
+}
+
+async function createTable() {
+    try {
+        const tableExists = await knexdb.schema.hasTable(tableName);
+        if (tableExists) {
+            console.log(`Table ${tableName} already exists`);
+            return;
+        }
+        // Create the table if it doesn't exist
+        await knexdb.schema.createTable(tableName, (table) => {
+            headers.forEach((header) => {
+                table.string(header);
+            });
+        });
+        console.log(`Table ${tableName} created successfully`);
+        console.log(`Headers: ${headers}`);
+    } catch (error) {
+        throw new Error(`Error creating table: ${error.message}`, error);
+    }
+
+};
+
+async function insertData() {
+    try {
+        await knexdb(tableName).insert(rows);
+        console.log(`Data inserted successfully into ${tableName}`);
+        console.log(`Inserted ${rows.length} rows`);
+    } catch (error) {
+        throw new Error(`Error inserting data: ${error.message}`, error);
+    }
 };
 
 main();
-
